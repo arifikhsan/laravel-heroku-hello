73f584cdd6fcbc15bdd90260c73574d119eba420
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-07 11:19:12 +0700

    auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Schema::defaultStringLength(191);
+
         DB::listen(function ($query) {
             Log::info(
                 $query->sql,
